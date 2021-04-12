03cce2fff6fc600b2e290d0f2664b24944d8f0d2
gabrielmagda <gabriel.magda@clickandpledge.com>
2021-04-12 13:01:21 +0300

    added env variables

--- keys.js
+++
@@ -1,5 +0,0 @@
-// API Keys - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = '';
-
-// Context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = '';
--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Header from '../components/Header';
-import { API_KEY, CONTEXT_KEY } from '../keys';
 import { response } from '../response';
 import { useRouter } from 'next/router';
 import SearchResults from '../components/SearchResults';
@@ -29,7 +28,7 @@ export async function getServerSideProps(context) {
     const useDummyData = false;
     const startIndex = context.query.start || '0';
 
-    const data = useDummyData ? response : await fetch(`https://customsearch.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
+    const data = useDummyData ? response : await fetch(`https://customsearch.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
     .then(response => response.json());
 
     // After the server has rendered, pass the results to the client
