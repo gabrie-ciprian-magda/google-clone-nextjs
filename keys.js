d86622f14b02f863eccf767a2fade32ab76b7a96
gabrielmagda <gabriel.magda@clickandpledge.com>
2021-04-12 11:50:04 +0300

    adding vercel keys

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Keys - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = '@api_key';
+
+// Context key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = '@context-key';
