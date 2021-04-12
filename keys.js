5954a1aee4c7bb277b3404d86f6f87e953179d68
gabrielmagda <gabriel.magda@clickandpledge.com>
2021-04-12 12:00:25 +0300

    adding vercel json file

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // API Keys - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = '@api_key';
+export const API_KEY = '';
 
 // Context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = '@context-key';
+export const CONTEXT_KEY = '';
