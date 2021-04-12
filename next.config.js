fac052cbb4054727a012c7916fb0da059b566a17
gabrielmagda <gabriel.magda@clickandpledge.com>
2021-04-12 15:31:50 +0300

    added both keys

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
         domains: ['www.google.com']
     },
     env: {
-        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY
+        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+        NEXT_PUBLIC_CONTEXT_KEY: process.env.NEXT_PUBLIC_CONTEXT_KEY
     }
 }
